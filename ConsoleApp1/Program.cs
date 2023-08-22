c10a75aabc45ace77484ac89ab4819ca6617d99c
Dominik <domdbe@gmail.com>
2023-08-22 20:02:18 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿var myName = "Dominik";
+var myGender = "Male";
+var myAge = 26;
+
+if (myGender == "Female" && myAge > 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (myGender == "Female" && myAge == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (myGender == "Male" && myAge < 18)
+{
+    Console.WriteLine("NIepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Jestem Dominik, mam 26 lat");
+}
