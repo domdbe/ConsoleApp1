d0ad77b8a2d537ed6fc2108d155b435358d9620c
Dominik <domdbe@gmail.com>
2023-08-22 21:09:35 +0200

    If zadanie

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,11 @@
 var myGender = "Male";
 var myAge = 26;
 
-if (myGender == "Female" && myAge > 30)
+if (myGender == "Female" && myAge < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (myGender == "Female" && myAge == 33)
+else if (myName == "Ewa" && myAge == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
@@ -14,7 +14,7 @@ else if (myGender == "Male" && myAge < 18)
 {
     Console.WriteLine("NIepełnoletni Mężczyzna");
 }
-else
+else 
 {
-    Console.WriteLine("Jestem Dominik, mam 26 lat");
+    Console.WriteLine("Ktoś inny");
 }
